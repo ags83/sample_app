c5db670341d43457614a0511043e9dc3cdda5d66
Aaron Shaw <a.shaw83@Hotmail.com>
2011-07-12 17:19:46 +0700

    If you dont work ill kill you

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -21,4 +21,9 @@ describe "LayoutLinks" do
     get '/help'
     response.should have_selector('title', :content => "Help")
   end
+  
+  it "should have a Signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign Up")
+  end
 end
