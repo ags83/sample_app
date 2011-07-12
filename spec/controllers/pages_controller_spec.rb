9298c33b69351833d3443a918450f974b3ea0cac
Aaron Shaw <a.shaw83@Hotmail.com>
2011-07-12 12:49:51 +0700

    new stuff

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,17 +1,22 @@
 require 'spec_helper'
 
 describe PagesController do
-render_views
+  render_views
+
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App" 
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-	 it "should have the right title" do
+
+    it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
+                                    :content => @base_title + " | Home")
     end
   end
 
@@ -20,35 +25,38 @@ render_views
       get 'contact'
       response.should be_success
     end
-	 it "should have the right title" do
+
+    it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Contact")
+                                    :content => @base_title + " | Contact")
     end
   end
-  
+
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
     end
-	 it "should have the right title" do
+
+    it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | About")
+                                    :content => @base_title + " | About")
     end
-  end
-  
-   describe "GET 'help'" do
+   end
+	
+  describe "GET 'help'" do
     it "should be successful" do
       get 'help'
       response.should be_success
     end
-	 it "should have the right title" do
+
+    it "should have the right title" do
       get 'help'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Help")
+                                    :content => @base_title + " | Help")
     end
   end
-
+  
 end
